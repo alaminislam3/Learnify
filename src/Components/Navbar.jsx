720a3b725515f951343cbea7ee6676d3ec383146
AlaminIslamOnu <alamin.ndc.17@gmail.com>
2025-06-15 23:55:20 +0600

    created navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,135 @@
-import React from 'react';
+import React from "react";
+import { Link, NavLink } from "react-router";
 
 const Navbar = () => {
-    return (
-        <div>
-            
+  const links = (
+    <>
+      <NavLink
+        to="/"
+        className={({ isActive }) =>
+          isActive
+            ? "text-blue-600 underline font-semibold"
+            : "text-gray-600 hover:text-blue-600 font-semibold"
+        }
+      >
+        Home
+      </NavLink>
+
+      <NavLink
+        to="/allarticles"
+        className={({ isActive }) =>
+          isActive
+            ? "text-blue-600 underline font-semibold"
+            : "text-gray-600 hover:text-blue-600 font-semibold"
+        }
+      >
+        All Articles
+      </NavLink>
+      <NavLink
+        to="/postarticle"
+        className={({ isActive }) =>
+          isActive
+            ? "text-blue-600 underline font-semibold"
+            : "text-gray-600 hover:text-blue-600 font-semibold"
+        }
+      >
+        Post Articles
+      </NavLink>
+
+      <NavLink
+        to="/myarticles"
+        className={({ isActive }) =>
+          isActive
+            ? "text-blue-600 underline font-semibold"
+            : "text-gray-600 hover:text-blue-600 font-semibold"
+        }
+      >
+        My Articles
+      </NavLink>
+    </>
+  );
+
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow gap-x-6"
+          >
+            {links}
+          </ul>
         </div>
-    );
+        <a className="btn btn-ghost text-xl">Learnify</a>
+        <span className="relative">
+          <svg
+            className="w-6 h-6 text-primary"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="M2 7c2-1 4-1 6 0s4 1 6 0 4-1 6 0v13c-2-1-4-1-6 0s-4 1-6 0-4-1-6 0V7z" />
+          </svg>
+          <span className="absolute w-2.5 h-2.5 bg-orange-500 rounded-full top-0 left-3 animate-ping"></span>
+        </span>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 gap-x-6">{links}</ul>
+      </div>
+      <div className="navbar-end mr-2">
+        <a className="btn">Login</a>
+      </div>
+      <div className="dropdown dropdown-end mr-2">
+        <div
+          tabIndex={0}
+          role="button"
+          className="btn btn-ghost btn-circle avatar"
+        >
+          <div className="w-10 rounded-full">
+            <img
+              alt="Tailwind CSS Navbar component"
+              src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+            />
+          </div>
+        </div>
+        <ul
+          tabIndex={0}
+          className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+        >
+          <Link to={'/myarticles'}>
+             My Article
+          </Link>
+          <Link to={'/postarticle'}>
+             Post Article
+          </Link>
+          <Link>
+             Logout
+          </Link>
+          
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,22 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router";
+import MainLayout from './Layout/MainLayout';
+import Home from './Pages/Home';
+import AllArticles from './Pages/AllArticles';
+import PostArticle from './Pages/PostArticle';
+import MyArticles from './Pages/MyArticles';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Good Night Alamin . Do Fast..</div>,
+    Component: MainLayout,
+    children: [
+      {index: true , Component: Home},
+      {path: '/allarticles' , Component: AllArticles},
+      {path: '/postarticle' , Component: PostArticle},
+      {path: '/myarticles' , Component: MyArticles},
+    ]
   },
 ]);
 
