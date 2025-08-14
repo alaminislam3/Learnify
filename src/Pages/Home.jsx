f5eba974a874a3d60676496bbb4be3fc94a54be9
Alamin Islam <alamin.ndc.17@gmail.com>
2025-08-14 21:16:28 +0600

    added smart animation scrolling in everywhere

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,23 +1,38 @@
-import React from 'react';
-import Hero from '../Components/Hero';
-import ArticleCard from '../Components/ArticleCard';
-import CategoryList from '../Components/CategoryList';
-import Community from '../Components/Community';
-import Resources from '../Components/Resources';
+// Home.jsx
+import React, { useEffect } from "react";
+import Lenis from "@studio-freight/lenis";
+import Hero from "../Components/Hero";
+import ArticleCard from "../Components/ArticleCard";
+import CategoryList from "../Components/CategoryList";
+import Community from "../Components/Community";
+import Resources from "../Components/Resources";
 
 const Home = () => {
-    return (
-        <div>
-            <Hero></Hero>
-            <div className='container mx-auto px-6 sm:px-6 md:px-12 lg:px-24'>
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 0.5, // scroll animation duration
+      easing: (t) => t, // easing function
+      smooth: true,
+    });
 
-            <ArticleCard></ArticleCard>
-            <CategoryList></CategoryList>
-            <Community></Community>
-            <Resources></Resources>
-            </div>
-        </div>
-    );
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  }, []);
+
+  return (
+    <div>
+      <Hero />
+      <div className="container mx-auto px-6 sm:px-6 md:px-12 lg:px-24">
+        <ArticleCard />
+        <CategoryList />
+        <Community />
+        <Resources />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
